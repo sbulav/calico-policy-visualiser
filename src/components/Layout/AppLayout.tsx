24edaa41b289673eba825eae0089752dc9eedbe5
Sergei Bulavintsev <bulavintsev.sergey@gmail.com>
2026-02-18 11:41:38 +0300

    fix: amend app name in UI

--- src/components/Layout/AppLayout.tsx
+++ src/components/Layout/AppLayout.tsx
@@ -165,7 +165,7 @@ export default function AppLayout() {
             <svg className="w-6 h-6 text-indigo-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z" />
             </svg>
-            <h1 className="text-sm font-bold text-slate-100">Calico Network Policy Visualizer <span className="text-xs font-normal text-slate-300">v{__APP_VERSION__}</span></h1>
+            <h1 className="text-sm font-bold text-slate-100">Calico Network Policy Visualiser <span className="text-xs font-normal text-slate-300">v{__APP_VERSION__}</span></h1>
           </div>
 
           {/* Examples button */}
